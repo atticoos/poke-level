853d7110801c3205ccf2f1d1724d4e7a81d44bfb
Atticus White <contact@atticuswhite.com>
2016-07-19 00:29:00 -0400

    slider logic bylines

--- app/components/sliderGroup.js
+++ app/components/sliderGroup.js
@@ -3,27 +3,34 @@ import PokeSlider from './slider';
 import {EvolutionCost} from '../constants/pokemon';
 
 const candiesNeeded = (type, count) => count * EvolutionCost[type];
+const catchesNeeded = (type, caught, candies) => Math.abs(Math.min(0, Math.floor((candies - (caught * EvolutionCost[type])) / EvolutionCost[type])));
 
 export default class PokemonSliderGroup extends React.Component {
   render() {
+    const {
+      pokemonCount,
+      candyCount,
+      type
+    } = this.props;
     return (
       <div>
-        <h3>{this.props.type}</h3>
+        <h3>{type}</h3>
         <div className="group">
-          <h4># of {this.props.type}</h4>
+          <h4># of {type}</h4>
           <div>
             <PokeSlider
-              value={this.props.pokemonCount}
+              value={pokemonCount}
               onChange={this.props.pokemonCountChanged}
             />
           </div>
         </div>
         <div className="group">
-          <h4>{this.props.candyCount} of {candiesNeeded(this.props.type, this.props.pokemonCount)} of candies needed</h4>
+          <h4>{this.props.candyCount} of {candiesNeeded(type, pokemonCount)} of candies needed</h4>
+          <p>(catch {catchesNeeded(type, pokemonCount, candyCount)} more!)</p>
           <div>
             0
             <PokeSlider
-              value={this.props.candyCount}
+              value={candyCount}
               onChange={this.props.candyCountChanged}
               max={500}
             />
